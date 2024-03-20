c20b9690a6fadc95190c6ed19acb3ed4fa472c04
Steven <steven.t@outlook.dk>
2024-03-20 16:26:06 +0100

    update entrypoint for neptus

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Basic entrypoint for ROS / catkin Docker containers
 
+# Set the prompt color
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+
 # Source ROS 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 echo "Sourced ROS ${ROS_DISTRO}"
